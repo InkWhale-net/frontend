285f6812edf4882b81800946f2d6401959d9e822
huypham <huypham@microbox.tech>
2023-06-27 16:39:54 +0700

    fix decimal and display information

--- src/pages/farms/detail/index.jsx
+++ src/pages/farms/detail/index.jsx
@@ -56,6 +56,7 @@ import lp_pool_contract from "utils/contracts/lp_pool_contract";
 import nft_pool_contract from "utils/contracts/nft_pool_contract";
 import psp22_contract from "utils/contracts/psp22_contract";
 import psp34_standard from "utils/contracts/psp34_standard";
+import PoolInfo from "./PoolInfor";
 
 export default function FarmDetailPage() {
   const params = useParams();
@@ -370,7 +371,7 @@ const MyStakeRewardInfoNFT = ({
       currentAccount?.address
     );
 
-    const balance = formatQueryResultToNumber(result);
+    const balance = formatQueryResultToNumber(result, tokenDecimal);
     setTokenBalance(balance);
   }, [currentAccount?.address, currentAccount?.balance, tokenContract]);
 
@@ -1233,167 +1234,6 @@ const MyStakeRewardInfoToken = ({
             </Flex>
           </IWCard>
         </CardThreeColumn>
-      </Stack>
-    </>
-  );
-};
-
-const PoolInfo = ({
-  mode,
-  nftInfo,
-  poolContract,
-  startTime,
-  duration,
-  rewardPool,
-  totalStaked,
-  maxStakingAmount,
-  tokenSymbol,
-  tokenName,
-  tokenContract,
-  multiplier,
-  tokenTotalSupply,
-  lptokenContract,
-  lptokenDecimal,
-  lptokenName,
-  lptokenSymbol,
-  lptokenTotalSupply,
-  ...rest
-}) => {
-  const cardDataPoolInfo = {
-    cardHeaderList: [
-      {
-        name: "collectionLink",
-        hasTooltip: false,
-        tooltipContent: "",
-        label: "ArtZero Collection Link",
-      },
-      {
-        name: "totalSupply",
-        hasTooltip: false,
-        tooltipContent: "",
-        label: "NFT Supply",
-      },
-      {
-        name: "royaltyFee",
-        hasTooltip: false,
-        tooltipContent: "",
-        label: "Royalty Fee",
-      },
-      {
-        name: "volume",
-        hasTooltip: false,
-        tooltipContent: "",
-        label: "Volume",
-      },
-    ],
-
-    cardValue: {
-      collectionLink: (
-        <Link
-          isExternal
-          href={`https://a0.artzero.io/collection/${nftInfo?.nftContractAddress}`}
-        >
-          {nftInfo?.name}
-        </Link>
-      ),
-      volume: `${formatNumDynDecimal(nftInfo?.volume)} AZERO`,
-      totalSupply: `${nftInfo?.nft_count} NFT${nftInfo?.nft_count > 1 && "s"}`,
-      royaltyFee: `${(nftInfo?.royaltyFee / 100).toFixed(2)}%`,
-    },
-  };
-
-  return (
-    <>
-      {mode === "NFT_FARM" ? (
-        <NFTBannerCard cardData={cardDataPoolInfo} nftInfo={nftInfo} />
-      ) : null}
-
-      <Stack
-        w="full"
-        spacing="30px"
-        alignItems="start"
-        direction={{ base: "column", lg: "row" }}
-      >
-        <CardTwoColumn
-          title="General Information"
-          data={[
-            {
-              title: "Pool Contract Address",
-              content: <AddressCopier address={poolContract} />,
-            },
-            {
-              title: "Multiplier",
-              content:
-                mode === "TOKEN_FARM"
-                  ? (multiplier / 10 ** 18).toFixed(2)
-                  : (multiplier / 10 ** 12).toFixed(2),
-            },
-            {
-              title: "Start Date",
-              content: `${new Date(startTime).toLocaleString("en-US")}`,
-            },
-            {
-              title: "Pool Length",
-              content: `${duration / 86400} day${
-                duration / 86400 > 1 ? "s" : ""
-              }`,
-            },
-            {
-              title: "Reward Pool",
-              content: `${formatNumDynDecimal(rewardPool)} ${tokenSymbol}`,
-            },
-            {
-              title: "Max Staking Amount",
-              content: `${formatNumDynDecimal(maxStakingAmount)} ${"NFT"}${
-                mode === "NFT_FARM" && maxStakingAmount > 1 ? "s" : ""
-              }`,
-            },
-            {
-              title: "Total Value Locked",
-              content: `${formatNumDynDecimal(totalStaked)} ${"NFT"}${
-                mode === "NFT_FARM" && totalStaked > 1 ? "s" : ""
-              }`,
-            },
-          ]}
-        />
-        <Stack w="full" spacing="30px">
-          {mode === "TOKEN_FARM" ? (
-            <CardTwoColumn
-              title="Staking Token Information"
-              data={[
-                {
-                  title: "Total Name",
-                  content: lptokenName,
-                },
-                {
-                  title: "Contract Address",
-                  content: <AddressCopier address={lptokenContract} />,
-                },
-                {
-                  title: "Total Supply",
-                  content: `${formatNumDynDecimal(lptokenTotalSupply)}`,
-                },
-                { title: "Token Symbol", content: lptokenSymbol },
-              ]}
-            />
-          ) : null}
-
-          <CardTwoColumn
-            title="Reward Token Information"
-            data={[
-              { title: "Total Name", content: tokenName },
-              {
-                title: "Contract Address",
-                content: <AddressCopier address={tokenContract} />,
-              },
-              {
-                title: "Total Supply",
-                content: `${formatNumDynDecimal(tokenTotalSupply)}`,
-              },
-              { title: "Token Symbol", content: tokenSymbol },
-            ]}
-          />
-        </Stack>
       </Stack>
     </>
   );
