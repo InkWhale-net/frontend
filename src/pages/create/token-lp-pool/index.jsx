d6be767856b36224eb0c571dd98bfa905f8c1601
tianNg20 <thien10701@gmail.com>
2023-10-04 16:41:45 +0700

    fix: lp poool contract

--- src/pages/create/token-lp-pool/index.jsx
+++ src/pages/create/token-lp-pool/index.jsx
@@ -311,7 +311,7 @@ export default function CreateTokenLPPage({ api }) {
       LPtokenContract,
       selectedContractAddr,
       formatNumToBN(maxStake, tokenLPSymbol?.decimal || 12),
-      Number(multiplier),
+      Number(multiplier * 10000),
       roundUp(duration * 24 * 60 * 60 * 1000, 0),
       startTime.getTime()
     );
