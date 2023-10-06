e802e4403d4e3bc84f2920c8fec6544fe575b0bd
huypham <huyp26102000@gmail.com>
2023-10-06 09:14:24 +0700

    fix connect wallet overlapse

--- src/components/wallet/WalletButton.js
+++ src/components/wallet/WalletButton.js
@@ -54,7 +54,7 @@ export default function WalletButton({ onCloseSidebar }) {
       <WalletModal isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
 
       {!currentAccount ? (
-        <WalletNotConnect />
+        <WalletNotConnect onClose={onCloseSidebar} />
       ) : (
         <WalletConnect onClose={onCloseSidebar} onClickSwitch={onClickSwitch} />
       )}
