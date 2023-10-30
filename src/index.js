072c45172270f7b84294c1974ca7f78fa4f72e9a
HuyPham <huyp26102000@gmail.com>
2023-10-30 14:44:41 +0700

    enable lp

--- src/index.js
+++ src/index.js
@@ -191,7 +191,7 @@ const App = () => {
             component={CreateStakePoolPage}
           />
           <Route exact path={`/create/nft-lp`} component={CreateNFTLPPage} />
-          {/* <Route
+          <Route
             exact
             path={`/launchpad/create`}
             component={CreateLaunchpadPage}
@@ -201,7 +201,7 @@ const App = () => {
             exact
             path={`/launchpad/:launchpadContract`}
             component={PublicDetailLaunchpad}
-          /> */}
+          />
           <Route exact path={`/create/farming`} component={CreateTokenLPPage} />
           <Route exact path={`/farming`} component={LPPoolsPage} />
           <Route
