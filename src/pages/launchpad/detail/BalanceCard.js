4ddcb2437502f98dd88dd54acd8aa0342b03cec5
j <langdon0003@gmail.com>
2023-11-16 15:29:00 +0700

    Update Sale phases & My Purchase tab

--- src/pages/launchpad/detail/BalanceCard.js
+++ src/pages/launchpad/detail/BalanceCard.js
@@ -1,6 +1,7 @@
 import { Box, Divider, Heading, Text } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useSelector } from "react-redux";
+import { formatChainStringToNumber } from "utils";
 import { formatNumDynDecimal, formatTokenAmount, roundUp } from "utils";
 import { execContractQuery } from "utils/contracts";
 import psp22_contract from "utils/contracts/psp22_contract";
@@ -58,33 +59,145 @@ const BalanceCard = ({ launchpadData }) => {
   useEffect(() => {
     if (currentAccount) getBalance();
   }, [currentAccount, launchpadData]);
+
+  const tokenDecimal = launchpadData?.projectInfo?.token?.decimals;
+  const tokenSymbol = launchpadData?.projectInfo?.token?.symbol;
+
+  const totalSupply =
+    formatChainStringToNumber(launchpadData?.totalSupply) /
+    Math.pow(10, tokenDecimal);
+
+  const availableAmount =
+    formatChainStringToNumber(launchpadData?.availableTokenAmount) /
+    Math.pow(10, tokenDecimal);
+
+  const totalWhitelistByPhase = launchpadData?.phaseList?.map((p) => {
+    const totalSoldAmount = p?.whitelist?.reduce(
+      (prev, curr) =>
+        prev +
+        formatChainStringToNumber(curr.purchasedAmount) /
+          Math.pow(10, tokenDecimal),
+      0
+    );
+    return { ...p, totalSoldAmount };
+  });
+
+  const totalWhitelist = launchpadData?.phaseList?.reduce((prev, curr) => {
+    return prev.concat(curr?.whitelist);
+  }, []);
+
+  const formattedTotalWhitelist = useMemo(
+    () =>
+      totalWhitelist?.map((w) => ({
+        ...w,
+        amount:
+          formatChainStringToNumber(w?.amount) / Math.pow(10, tokenDecimal),
+        claimedAmount:
+          formatChainStringToNumber(w?.claimedAmount) /
+          Math.pow(10, tokenDecimal),
+        price: formatChainStringToNumber(w?.price) / Math.pow(10, tokenDecimal),
+        purchasedAmount:
+          formatChainStringToNumber(w?.purchasedAmount) /
+          Math.pow(10, tokenDecimal),
+        vestingAmount:
+          formatChainStringToNumber(w?.vestingAmount) /
+          Math.pow(10, tokenDecimal),
+      })),
+    [totalWhitelist, tokenDecimal]
+  );
+
+  const totalSoldAmount = formattedTotalWhitelist?.reduce(
+    (prev, curr) => prev + curr.purchasedAmount,
+    0
+  );
+
+  const totalWhitelistAddedAmount = formattedTotalWhitelist?.reduce(
+    (prev, curr) => prev + curr.amount,
+    0
+  );
+
+  const totalWhitelistClaimed = formattedTotalWhitelist?.filter(
+    (w) => !!w.claimedAmount
+  );
+
   return (
-    <Box
-      sx={{
-        marginTop: "12px",
-        border: "2.8px solid #E3DFF3",
-        borderRadius: "8px",
-        paddingTop: "20px",
-        paddingLeft: "16px",
-        paddingRight: "16px",
-        paddingBottom: "12px",
-      }}
-    >
-      <Heading size="md" sx={{ marginBottom: "4px" }}>
-        Your balance
-      </Heading>
-      <Divider
+    <>
+      <Box
         sx={{
-          marginBottom: "8px",
+          marginTop: "12px",
+          border: "2.8px solid #E3DFF3",
+          borderRadius: "8px",
+          paddingTop: "20px",
+          paddingLeft: "16px",
+          paddingRight: "16px",
+          paddingBottom: "12px",
         }}
-      />
-      <Row label="INW" value={currentAccount?.balance?.inw} />
-      <Row label="AZERO" value={currentAccount?.balance?.azero} />
-      <Row
-        label={launchpadData?.projectInfo?.token?.name}
-        value={tokenBalance || 0}
-      />
-    </Box>
+      >
+        <Heading size="md" sx={{ marginBottom: "4px" }}>
+          Your balance
+        </Heading>
+        <Divider
+          sx={{
+            marginBottom: "8px",
+          }}
+        />
+        <Row label="INW" value={currentAccount?.balance?.inw} />
+        <Row label="AZERO" value={currentAccount?.balance?.azero} />
+        <Row
+          label={launchpadData?.projectInfo?.token?.name}
+          value={tokenBalance || 0}
+        />
+      </Box>
+      <Box
+        sx={{
+          marginTop: "12px",
+          border: "2.8px solid #E3DFF3",
+          borderRadius: "8px",
+          paddingTop: "20px",
+          paddingLeft: "16px",
+          paddingRight: "16px",
+          paddingBottom: "12px",
+        }}
+      >
+        <Heading size="md" sx={{ marginBottom: "4px" }}>
+          Launchpad Balance
+        </Heading>
+        <Divider
+          sx={{
+            marginBottom: "8px",
+          }}
+        />
+        <Row
+          label="Total Token For Sale"
+          value={`${totalSupply} ${tokenSymbol}`}
+        />
+        <Row
+          label="Available Amount"
+          value={`${availableAmount} ${tokenSymbol}`}
+        />
+        <Row
+          label="Total Whitelist Added"
+          value={`${totalWhitelistAddedAmount} ${tokenSymbol}`}
+        />
+        <Row label="Total Sold" value={`${totalSoldAmount} ${tokenSymbol}`} />
+
+        {totalWhitelistByPhase?.map((p, idx) => (
+          <Row
+            key={idx}
+            label={` - ${p?.name}`}
+            value={`${p.totalSoldAmount} ${tokenSymbol}`}
+          />
+        ))}
+        <Row
+          label="Total Whitelist Added"
+          value={`${formattedTotalWhitelist?.length} address(es)`}
+        />
+        <Row
+          label="Total Whitelist Claimed"
+          value={`${totalWhitelistClaimed?.length} address(es)`}
+        />
+      </Box>
+    </>
   );
 };
 
