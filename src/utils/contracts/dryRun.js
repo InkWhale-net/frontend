a6f3de952a58a5d878b9197358a012318d4d52d1
j <langdon0003@gmail.com>
2023-11-22 17:17:27 +0700

    add calc estimated gas

--- src/utils/contracts/dryRun.js
+++ src/utils/contracts/dryRun.js
@@ -1,5 +1,6 @@
 import { BN } from "@polkadot/util";
 import { convertWeight } from "@polkadot/api-contract/base/util";
+import { formatChainStringToNumber } from "utils";
 
 const toContractAbiMessage = (contractPromise, message) => {
   const value = contractPromise.abi.messages.find((m) => m.method === message);
@@ -51,5 +52,53 @@ export const getGasLimit = async (
     proofSize: v2Weight.proofSize,
   });
 
+  // const rs =
+  //   formatChainStringToNumber(gasRequired.toHuman()?.refTime) /
+  //   Math.pow(10, 12);
+
+  // console.log(message, "getGasLimit NORMAL ", gasMul, "x refTime", rs);
+
   return { ok: true, value: gasRequired };
+};
+
+export const getSwapGasLimit = async (
+  api,
+  userAddress,
+  message,
+  contract,
+  options = {},
+  args = []
+) => {
+  try {
+    const abiMessage = toContractAbiMessage(contract, message);
+    if (!abiMessage.ok) return abiMessage;
+
+    const { value, gasLimit, storageDepositLimit } = options;
+
+    const result = await api.call.contractsApi.call(
+      userAddress,
+      contract.address,
+      value ?? new BN(0),
+      gasLimit ?? null,
+      storageDepositLimit ?? null,
+      abiMessage.value.toU8a(args)
+    );
+
+    const { v2Weight } = convertWeight(result.gasRequired);
+
+    const gasRequired = api.registry.createType("WeightV2", {
+      refTime: v2Weight.refTime.mul(new BN(1)),
+      proofSize: v2Weight.proofSize.mul(new BN(1)),
+    });
+
+    const rs =
+      formatChainStringToNumber(gasRequired.toHuman()?.refTime) /
+      Math.pow(10, 12);
+
+    // console.log(message, "getSwapGasLimit ESTIMATED 1x refTime", rs);
+
+    return rs;
+  } catch (error) {
+    console.log("error", error);
+  }
 };
