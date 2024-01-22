7ca5849426f0a641b3880e24177684b1ed7e9c6e
j <langdon0003@gmail.com>
2024-01-22 16:10:48 +0700

    fix [Pretest] : Không remove reward được do làm tròn #346

--- src/pages/account/my-pools/detail/index.jsx
+++ src/pages/account/my-pools/detail/index.jsx
@@ -664,7 +664,7 @@ const MyPoolInfo = ({
 
   const setMaxReward = () => {
     if (roundUp(withdrawbleAm) > 0.0001) {
-      setAmount(withdrawbleAm);
+      setAmount(withdrawbleAm.toFixed(3));
     }
   };
 
