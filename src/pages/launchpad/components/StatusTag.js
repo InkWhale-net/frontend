9b059c4908eda2a4aa9183fb65b058eb47b91266
Pham Huy <76714933+huyp26102000@users.noreply.github.com>
2024-02-03 04:14:49 +0700

    update status #328

--- src/pages/launchpad/components/StatusTag.js
+++ src/pages/launchpad/components/StatusTag.js
@@ -1,3 +1,4 @@
+import { Tooltip } from "@chakra-ui/react";
 import { BsDot } from "react-icons/bs";
 
 export const RequireKyc = () => {
@@ -18,6 +19,29 @@ export const RequireKyc = () => {
     >
       KYC
     </div>
+  );
+};
+export const DoxxedTag = () => {
+  return (
+    <Tooltip label="At least one of the team members’ identity is verified.">
+      <div
+        style={{
+          background: "#93F0F5",
+          paddingLeft: "12px",
+          paddingRight: "12px",
+          borderRadius: "20px",
+          display: "flex",
+          alignItems: "center",
+          fontWeight: "bold",
+          fontSize: "14px",
+          color: "#57527E",
+          marginRight: "4px",
+        }}
+      >
+        DOXXED
+      </div>
+    </Tooltip>
+
   );
 };
 export const UpcomingStatusTag = () => {
