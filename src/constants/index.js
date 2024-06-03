92bf0e8036a9e5146c1d4b2c6c50beed577b7013
Albert Tran <alberttran0810@gmail.com>
2024-06-03 15:07:22 +0700

    Hotfix: change rpc of 5ire

--- src/constants/index.js
+++ src/constants/index.js
@@ -165,7 +165,7 @@ export const supportedChain = [
     key: "firechain-testnet",
     icon: Icon5Ire,
     decimal: 18,
-    providerURL: "wss://wss-testnet.5ire.network",
+    providerURL: "wss://wss.5ire.network",
     unit: "5IRE",
     inwName: "INW",
     url: "https://5iretest.inkwhale.net/",
--- src/utils/contracts/firechain/index.js
+++ src/utils/contracts/firechain/index.js
@@ -17,7 +17,7 @@
 // import moment from "moment";
 
 // // ========================5ire chain balance=====================================
-// const provider = new WsProvider("wss://wss-testnet.5ire.network");
+// const provider = new WsProvider("wss://wss.5ire.network");
 // const fireApi = await ApiPromise.create({
 //   provider,
 //   throwOnConnect: true,
